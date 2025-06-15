ef9ae67ae4693b29c24bb5b8685e39ffe67d66a5
iamshashanksharma <shashank16septd@gmail.com>
2025-06-15 15:16:18 +0530

    Added primitivetypes

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ console.log(myName);
 4) industry standard camelcasing (eg: myName)
 5)cannot be reserved word
 */
+/*
+It got changed because it let keyword 
+
+let interestRate = 0.8;
+interestRate = 1;
+console.log(interestRate);
+*/
+
+const interestRate = 0.8;
+// interestRate = 1;  It gives type error as const cannot be reassigned 
+console.log(interestRate);
+
+// types in js 
+
+/*
+Primitives(value) :
+string , number , boolean , null , undefined
+
+reference :
+*/
+
+let friendName = "Raj";
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof(friendName)); // string
+console.log(typeof(age));        // number 
+console.log(typeof(isApproved)); // boolean
+console.log(typeof(firstName));  // undefined 
+console.log(typeof(selectedColor)); // object shocked welcome to js typeof(null) = object
+
