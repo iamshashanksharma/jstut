1fa19579353736e93d6db0828a2bef7dd1803569
iamshashanksharma <shashank16septd@gmail.com>
2025-06-15 14:51:58 +0530

    variablerules

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Hello world');
+let myName = "shashank";
+console.log(myName);
+
+// variable naming rules 
+
+/*
+1) cannot start with number
+2) cannnot have hyphen and space between varible naming
+3) meaningful
+4) industry standard camelcasing (eg: myName)
+5)cannot be reserved word
+*/
